62288aa4622eb0bb62383a4b68be54bbe94e1438
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-05-04 17:24:02 +0300

    ct

--- src/utils/createMyProfile.js
+++ src/utils/createMyProfile.js
@@ -3,7 +3,7 @@ import { api } from "./api";
 export async function createMyProfile(pupilOrTeacher, { name, password }) {
   try {
     if (!pupilOrTeacher) {
-      return await api.post(`/teacher`, {
+      return await api.post(`http://176.223.141.42:8080/teacher`, {
         name,
         password,
         role: "teacher",
