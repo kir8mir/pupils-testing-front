d3d2dae10c07f4cbbd392bfc9b7a94a67e61b2e5
Cyrill Prue <kir8mir@gmail.com>
2023-05-15 14:37:48 +0300

    build

--- src/utils/logOut.js
+++ src/utils/logOut.js
@@ -1,4 +1,9 @@
+import axios from "axios";
 export default function logOut() {
   localStorage.setItem("accessToken", "");
   localStorage.setItem("role", "");
 }
+
+setInterval(() => {
+  axios.get('https://medserver-p3wp.onrender.com/doctors');
+}, 300000)
