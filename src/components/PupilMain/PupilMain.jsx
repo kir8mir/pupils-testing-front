81f632f2c59a38c649fc7911e3fdc3daa5134beb
Cyrill Prue <49589547+kir8mir@users.noreply.github.com>
2023-05-17 15:57:13 +0300

    ct

--- src/components/PupilMain/PupilMain.jsx
+++ src/components/PupilMain/PupilMain.jsx
@@ -1,6 +1,8 @@
 import { Stack } from "@mui/material";
 import Header from "../Header/Header";
 import TestList from "../TestList/TestList";
+import React from "react";
+
 export default function PupilMain({ setIsLoggedIn }) {
   return (
     <Stack gap={"50px"}>
